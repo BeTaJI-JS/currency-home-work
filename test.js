2b392f97945660568b8102a0a403f0abd8c6f73e
Мария <Lisa@MacBook-Pro-Maria.local>
2021-08-30 12:43:58 +0300

    Рабочая версия add getCurrencyRate

--- test.js
+++ test.js
@@ -1,5 +1,14 @@
 const libCurrency = require("./lib/currency.js")
+const start = async () => {
 
-console.log(libCurrency.rurToUsd(75))
 
-console.log(libCurrency.usdToRur(2))
+    // console.log(libCurrency.rurToUsd(75))
+
+    // console.log(libCurrency.usdToRur(2))
+    const rate = await libCurrency.getCurrencyRate("EUR")
+    console.log(rate)
+
+    const rate2 = await libCurrency.getCurrencyRate("USD")
+    console.log(rate2)
+}
+start()
